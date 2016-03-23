970c8b5af7cbd70a0120975d8ba17f11e2aad47f
Maddy Tennant <maddy@Macintosh.local>
2016-03-23 20:43:34 +1300

    Start of PizzeriaTime

---
+++ PizzeriaTime/src/pizzeriatime/PizzeriaTime.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pizzeriatime;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author maddy
+ */
+public class PizzeriaTime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        console();
+    }
+
+    public static void console() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String username = scanner.next();
+        System.out.print("Please enter a contact number: ");
+        int number = scanner.nextInt();
+        System.out.print("Enter your address: ");
+        String address = scanner.next();
+        System.out.println(String.format("%s, please check the below details "
+                + "are correct \r\n"
+                + "Contact Number: %d \r\n"
+                + "Address: %s \r\n", username, number, address));
+
+//        Runtime runtime = Runtime.getRuntime();
+//        Process process = runtime.exec("ipconfig"); // you might need the full path
+//        InputStream is = process.getInputStream();
+//        InputStreamReader isr = new InputStreamReader(is);
+//        BufferedReader br = new BufferedReader(isr);
+//        String line;
+//        String login = br.readLine("Enter your login: ");
+//        char[] oldPassword = br.readPassword("Enter your old password: ");
+//        while ((line = br.readLine()) != null) {
+//        System.out.println(line);
+    }
+}
+
